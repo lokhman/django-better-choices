19b5c8888e62235472996f1ad603413eebd2c259
Alexander Lokhman <alex.lokhman@gmail.com>
2020-10-05 13:23:22 +0100

    Add removeprefix and removesuffix str methods for Python 3.9

--- django_better_choices/__init__.py
+++ django_better_choices/__init__.py
@@ -17,8 +17,8 @@ class _Value(str):
     capitalize = casefold = center = count = encode = endswith = expandtabs = find = format = format_map = \
         index = isalnum = isalpha = isascii = isdecimal = isdigit = isidentifier = islower = isnumeric = \
         isprintable = isspace = istitle = isupper = join = ljust = lower = lstrip = maketrans = partition = \
-        replace = rfind = rindex = rjust = rpartition = rsplit = rstrip = split = splitlines = startswith = \
-        strip = swapcase = title = translate = upper = zfill = property()
+        removeprefix = removesuffix = replace = rfind = rindex = rjust = rpartition = rsplit = rstrip = split = \
+        splitlines = startswith = strip = swapcase = title = translate = upper = zfill = property()
 
     def __new__(cls, display: Union[str, Promise], *, value: str = '', **params: Any):
         """
