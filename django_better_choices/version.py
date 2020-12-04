4968879146643b96afe692f0f7506643cb0e0c42
Alex Lokhman <alex.lokhman@gmail.com>
2020-12-04 01:58:29 +0000

    Format version module

--- django_better_choices/version.py
+++ django_better_choices/version.py
@@ -1,6 +1,8 @@
-from pkg_resources import get_distribution, DistributionNotFound
+"""https://github.com/lokhman/django-better-choices/releases"""
+
+import pkg_resources
 
 try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
-    __version__ = 'unknown'
+    __version__ = pkg_resources.get_distribution(__name__).version
+except pkg_resources.DistributionNotFound:
+    __version__ = "unknown"
